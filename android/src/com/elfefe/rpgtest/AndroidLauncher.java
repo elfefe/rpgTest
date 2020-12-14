1bd47051527aa79f82f72b4d977a1031b3d0eacc
FELIX BOULE--REIFF <fboule@deleo.fr>
2020-12-14 12:58:56 +0100

    First commit

---
+++ android/src/com/elfefe/rpgtest/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.elfefe.rpgtest;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.elfefe.rpgtest.RpgTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RpgTest(), config);
+	}
+}
---
+++ desktop/src/com/elfefe/rpgtest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.elfefe.rpgtest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.elfefe.rpgtest.RpgTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RpgTest(), config);
+	}
+}
